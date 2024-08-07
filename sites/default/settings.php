faca52a7275841e9b261a0e2515e19e55a186375
Hamza ERRAHMA <Hamza.ERRAHMA@um6p.ma>
2024-08-07 11:41:04 +0100

    database connection

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -899,8 +899,8 @@ $databases['default']['default'] = array (
   'port' => '3306',
   'isolation_level' => 'READ COMMITTED',
   'driver' => 'mysql',
-  'namespace' => 'Drupal\mysql\Driver\Database\mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+  #'namespace' => 'Drupal\mysql\Driver\Database\mysql',
+  #'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
 
 $settings['config_sync_directory'] = 'sites/default/files/config_78LY__Mpcf-6nOnTYzNsaDb44w0-Sp1q6Oxu_JJxnoi-HLCIGYHTdme0OJfIb5Jun5IQFahb-A/sync';
